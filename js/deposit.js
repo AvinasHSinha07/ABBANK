c1c37d73c89eaf19cce5e90eb3885419a40732ba
Avinash <avinashsinha751@gmail.com>
2023-08-17 02:08:37 +0600

    final

--- js/deposit.js
+++ js/deposit.js
@@ -1,30 +1,31 @@
-console.log("deposit")
+console.log("deposit");
 
-document.getElementById('depositBtn').addEventListener('click', function(){
-    
+document.getElementById("depositBtn").addEventListener("click", function () {
+  // console.log("deposit btn clicked")
+  const depositAmount = document.getElementById("depositField");
+  const newDeposit = parseFloat(depositAmount.value);
+  // console.log(typeof newDeposit)
+  depositAmount.value = "";
+  if (isNaN(newDeposit)) {
+    alert("please provide a number");
+    return;
+  }
 
-    // console.log("deposit btn clicked")
-    const depositAmount = document.getElementById('depositField')
-    const newDeposit = parseFloat(depositAmount.value) 
-    // console.log(typeof newDeposit)
+  const previousDeposit = document.getElementById("depositTotal");
+  const h = parseFloat(previousDeposit.innerText);
+  // console.log(typeof h)
 
-    const previousDeposit = document.getElementById('depositTotal')
-    const h = parseFloat(previousDeposit.innerText) 
-    // console.log(typeof h)
+  const totalDeposit = newDeposit + h;
+  // console.log(totalDeposit)
 
-    const totalDeposit = newDeposit + h
-    // console.log(totalDeposit)
+  previousDeposit.innerText = totalDeposit;
 
-    previousDeposit.innerText = totalDeposit;
-    depositAmount.value = ''
+  const totalBalance = document.getElementById("totalBalance");
+  const previousBalance = parseFloat(totalBalance.innerText);
+  console.log(previousBalance);
 
-    const totalBalance = document.getElementById('totalBalance')
-    const previousBalance =parseFloat(totalBalance.innerText) 
-    console.log(previousBalance)
+  const newBalance = previousBalance + newDeposit;
+  console.log(newBalance);
 
-    const newBalance = previousBalance + newDeposit
-    console.log(newBalance)
-
-    totalBalance.innerText = newBalance
-
-})
+  totalBalance.innerText = newBalance;
+});
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,36 +1,35 @@
 // console.log("withdraw")
 
 document.getElementById("withdrawBtn").addEventListener("click", function () {
-   
   // console.log('button clicked')
   const withdrawField = document.getElementById("withdrawField");
   const withdrawAmount = parseFloat(withdrawField.value);
 
+  withdrawField.value = "";
+  if (isNaN(withdrawAmount)) {
+    alert("please provide a number");
+    return;
+  }
   const withdrawInitial = document.getElementById("withdrawTotal");
   const withdrawTotal = withdrawInitial.innerText;
   const prevWithdraw = parseFloat(withdrawTotal);
-  
 
   const totalBalance = document.getElementById("totalBalance");
 
   const previousBalance = parseFloat(totalBalance.innerText);
 
-  console.log(previousBalance)
-  withdrawField.value = "";
-    if(withdrawAmount > previousBalance){
-        alert('low balance')
-        return
-    }
+  console.log(previousBalance);
+
+  if (withdrawAmount > previousBalance) {
+    alert("low balance");
+    return;
+  }
 
   const currentTotal = parseFloat(withdrawAmount) + parseFloat(withdrawTotal);
 
   withdrawInitial.innerText = currentTotal;
 
-  
-
   const newBalance = previousBalance - withdrawAmount;
 
   totalBalance.innerText = newBalance;
-
-
 });
